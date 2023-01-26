434cb833e511e75f21c5d42a045def0812c17133
MarkosMartinez <ikbdk@plaiaundi.net>
2023-01-26 12:14:09 +0100

    First Commit, clases creadas

---
+++ src/clases/Menu.java
@@ -0,0 +1,21 @@
+package clases;
+
+public class Menu {
+	
+	public static void menuPrincipal() {
+		
+	}
+	
+	public static void menuLibros() {
+		
+	}
+
+	public static void menuSocios() {
+	
+	}
+	
+	public static void menuPrestamos() {
+		
+	}
+
+}
